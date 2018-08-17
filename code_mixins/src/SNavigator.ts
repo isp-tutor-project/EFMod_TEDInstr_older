1c8d73b1796a936a4ebb1dcd3d71c82c8565bc61
synaptek <KevinWillows@gmail.com>
2018-08-17 19:21:35 -0400

    Initial commit

---
+++ code_mixins/src/SNavigator.ts
@@ -0,0 +1,89 @@
+
+
+namespace EFTut_Suppl.EFMOD_CODETEST {
+
+    export class SNavigator {
+
+        // This is a special signature to avoid the typescript error "because <type> has no index signature."
+        // on syntax like => this[<element name>]
+        // 
+        [key: string]: any;
+
+        
+        //***********************************************
+        // Tutor graph methods
+        //
+        
+        public $onCreateScene() {
+            console.log("$Navigator created");
+            this.connectNavButton(CONST.NEXTSCENE, "Snext");
+            this.connectNavButton(CONST.PREVSCENE, "Sback");
+            this.setNavigationTarget(CONST.NAVSCENE);            
+        }
+
+        public $onEnterScene() {
+
+        }
+        
+        public $preEnterScene() {
+        }
+
+        public $preExitScene() {
+        }
+
+        public $demoInitScene() {
+        }
+
+        public $logScene() {
+        }
+
+        public $rewindScene() {
+        }
+
+        public $resolveTemplate(templID:string) { 
+        
+            return this["$"+templID];
+        }
+
+
+        //***********************************************
+        // Scene graph methods
+        //
+        public $nodePreEnter(nodeId:string) {
+        }
+
+        public $nodePreExit(nodeId:string) {
+        }
+
+        public $nodeAction(actionId:string) : void {
+            
+            switch(actionId) {
+            }
+        }
+        
+        public $nodeConstraint(constrainId:string) : boolean {
+
+            let result:boolean = false;
+
+            return result;
+        }
+
+        // Track methods.
+        // 
+        public $cuePoints(trackID:string, cueID:string) {
+
+            switch(cueID) {
+                
+                case "$start":
+                    console.log("executing CuePoint START");
+                    break;
+                case "$end":
+                    console.log("executing CuePoint END");
+                    break;
+            }
+        }
+
+        public $timedEvents(id:string) {
+        }
+    }
+}
